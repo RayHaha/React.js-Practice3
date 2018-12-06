1f3ba6608dbe0358b5f8762a485e5d16b4fc8dca
RayHaha <g820324@gmail.com>
2018-12-06 14:19:25 -0800

    change frome uncontrolled to controlled elements

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,10 +4,13 @@ class SearchBar extends React.Component{
     // this function get called everytime someome changes the text inside the input
     // the name of the callback does not make a difference, you can use every name you want
     // and call by onChange
-    OnInputChange(event){
-        // we'll get the input by event.target.value
-        console.log(event.target.value);
-    }
+    // OnInputChange(event){
+    //     // we'll get the input by event.target.value
+    //     console.log(event.target.value);
+    // }
+
+    // change from uncontrolled elements to controlled elements
+    state = { term: '' };
 
     render(){
         return (
@@ -21,7 +24,13 @@ class SearchBar extends React.Component{
                         {/** if there is only one line to do in the function */}
                         {/** onChange={(event) => console.log(event.target.value)} */}
                         {/** or abbreviated onChange={(e) => console.log(e.target.value)} */}
-                        <input type="text" onChange={this.OnInputChange} />
+                        {/** <input type="text" onChange={this.OnInputChange} /> */}
+                        {/** change to controlled elements */}
+                        <input 
+                            type="text" 
+                            value={this.state.term} 
+                            onChange={e => this.setState({ term: e.target.value })} 
+                        />
                     </div>
                 </form>
             </div>
