360a8ee0ad0d205add871d9c29d4ae615ab84d85
RayHaha <g820324@gmail.com>
2018-12-07 17:07:49 -0800

    handling requests with async await

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,31 @@ import SearchBar from './SearchBar';
 // refactor function based App to a class based component
 // so we can use the function onSearchSubmit to get callback from children
 class App extends React.Component{
-    onSearchSubmit(term){
+    // handling request with async await
+    // axios.get will need to wait until the result get back so we use the async way to run
+    async onSearchSubmit(term){
         // use axios.get to search the photos
         // the first argument is the address
         // the second argument is an object that will have a bunch of options that will custimize this request
-        axios.get('https://api.unsplash.com/search/photos', {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID 2d5b213c5d7a96abf39c7ed377fc99af033cf497a6610cf5dd94b5150cf445ef'
             }
         });
+
+        console.log(response);
     }
+    // we can also write in this way
+    /**
+    onSearchSubmit(term){
+        axios.get('', {
+
+        }).then((response) => {
+            console.log(response);
+        });
+    }
+    */
 
     render(){
         return (
