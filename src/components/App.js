cd44e670c3ccabd4a0c54eb07b5aa29f5e82fcf3
RayHaha <g820324@gmail.com>
2018-12-07 16:31:50 -0800

    add axios.get

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 // refactor function based App to a class based component
 // so we can use the function onSearchSubmit to get callback from children
 class App extends React.Component{
     onSearchSubmit(term){
-        console.log(term);
+        // use axios.get to search the photos
+        // the first argument is the address
+        // the second argument is an object that will have a bunch of options that will custimize this request
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 2d5b213c5d7a96abf39c7ed377fc99af033cf497a6610cf5dd94b5150cf445ef'
+            }
+        });
     }
 
     render(){
