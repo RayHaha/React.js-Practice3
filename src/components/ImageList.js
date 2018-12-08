87989464366c864f9ab6c5561abb959d7c2399ab
RayHaha <g820324@gmail.com>
2018-12-07 21:15:46 -0800

    implementing keys and alts in lists

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -5,9 +5,16 @@ const ImageList = (props) => {
     // it means every image(= this.state.images = response.data.results) from props
     // will be map to the img tag
     // which src=image.urls.regular(= response.data.results.urls.regular)
-    const images = props.images.map((image) => {
-        return <img src={image.urls.regular} />;
+    // const images = props.images.map((image) => {
+    //     // we only have to assign the key to the root element that we return
+    //     // if there is <div> then it'll be <div key={}>
+    //     return <img alt={image.description} key={image.id} src={image.urls.regular} />;
+    // });
+    // we can also write in this way
+    const images = props.images.map(({ description, id, urls}) => {
+        return <img alt={description} key={id} src={urls.regular} />;
     });
+
     return <div>{images}</div>;
 }
 
