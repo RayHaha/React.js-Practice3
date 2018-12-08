1187d2a27cfc73a37cec906e2a763cb462c1bf73
RayHaha <g820324@gmail.com>
2018-12-07 20:54:27 -0800

    rendering lists of components

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    console.log(props.images);
-    return <div>ImageList</div>;
+    // use map method to create a new component
+    // it means every image(= this.state.images = response.data.results) from props
+    // will be map to the img tag
+    // which src=image.urls.regular(= response.data.results.urls.regular)
+    const images = props.images.map((image) => {
+        return <img src={image.urls.regular} />;
+    });
+    return <div>{images}</div>;
 }
 
 export default ImageList;
